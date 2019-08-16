079c79017a927debaaf92f41a5262478e5e743bb
Noah Anderson <noahbanderson@gmail.com>
2019-08-16 15:05:15 -0600

    Added game files

--- casino.rb
+++ casino.rb
@@ -1,2 +1,27 @@
 require "pry"
 require "colorize"
+require_relative "rps"
+
+class Casino
+
+    def initialize
+        menu
+        
+    end
+
+    def menu
+    puts "Welcome to Big Money Casino"
+    Rock_Paper_Scissor.new
+    
+
+    end
+end
+
+
+Casino.new
+
+
+
+
+
+
---
+++ rps.rb
@@ -0,0 +1,15 @@
+require "pry"
+require "colorize"
+
+
+class Rock_Paper_Scissor
+
+    def initialize
+        puts "Welcome to the Rock Paper Scissors game!"
+
+    end
+
+
+
+end
+
