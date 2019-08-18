d3dc6e7a7f89a82b057e558c676eebb8e7e38280
Jake Frank <jacob423frank@gmail.com>
2019-08-18 13:59:19 -0600

    progress

--- dice.rb
+++ dice.rb
@@ -1,3 +1,23 @@
 require "pry"
 require "colorize"
 require_relative "casino"
+
+# Pig is a deceptively simple dice game that anyone can learn in a snap.
+
+# Step 1: 
+# Roll one die for your first turn. Playing with two dice is optional.
+
+# Step 2: 
+# Enter points on the score sheet if the number rolled is two, three, four, five, or six points. If you roll a one, you lose all the points you accumulated during that turn, and you lose your turn.
+
+# Step 3: 
+# Continue rolling the die until you're happy with the number of points you've scored. Then, "hold" by handing the die over to your opponent, and track all points earned during your turn. 
+
+# Tip
+# Knowing when to stop rolling and end your turn is the key to winning at Pig. It's an exercise in deciding between gut instinct and greed. 
+
+# Step 4: 
+# Reach 100 points first and win the game. 
+
+# Step 5: 
+# To play with two dice, roll both for your first turn. If you roll a one on either die, you score zero for that turn and your turns ends, but if you roll two ones – or snake eyes – you lose your turn and all the points you've earned during the game.
--- slots.rb
+++ slots.rb
@@ -1,3 +1,124 @@
 require "pry"
 require "colorize"
 require_relative "casino"
+
+class Game
+  def initialize
+    @wallet = {}
+    @bet = {}
+
+  @numbers = [ "1", "2", "3" ]
+  
+    puts "--- SLOTS! SLOTS! SLOTS! ---"
+    puts 
+    puts
+    puts
+    sleep 1
+    puts "--- What is your name? ---"
+    @name = gets.strip
+    puts 
+    puts
+    puts "--- How Much Money Do You Have? ---"
+    @wallet = gets.to_i
+    
+    # @bet > @cash?
+    # then does not work
+    
+    puts
+    puts "--- Welcome #{@name}! You have $#{@wallet} ---"
+    puts 
+
+    
+    menu
+  end
+
+  def menu
+    puts "1) --- To Play Slots ---"
+    puts "2) --- Exit Big Money Casino ---"
+    # puts "3) --- Main Menu ---""
+    choice = gets.to_i
+    case choice 
+    when 1
+      # puts "--- PLACE YOUR BET ---"
+      # puts
+      # @Bet = gets.to_i
+      # puts
+      # puts "Bet Placed At $#{@Bet}"
+      # puts
+      # puts 
+      bet
+    when 2 
+      puts "--- GOODBYE! ---"
+      puts 
+      exit!
+    # when 3
+    #   puts "--- Thanks For Playing Slots! ---"
+      # main_menu or @main_menu
+    else
+      puts "Sorry #{@name}, You Are Too Drunk To Gamble."
+      puts
+      puts 
+      sleep 4
+      exit!
+      bet 
+    end
+  end
+  
+  def bet 
+    puts "--- PLACE YOUR BET ---"
+    puts
+    @Bet = gets.to_i
+    puts
+    puts "Bet Placed At $#{@Bet}"
+    puts
+    puts 
+    play_slots
+  end
+
+  def play_slots
+    
+    #  ------ go to bet method -----
+    # ----- in the bet method, 
+    # They win? @bet * three (whatever you want them to win)
+
+    # if they lose @wallet - @bet = new @wallet
+    #--------------------------------------------------------------------------
+    # get 3 nums and save them to variables
+    num_1 = @numbers.sample
+    num_2 = @numbers.sample
+    num_3 = @numbers.sample
+    # Display all 3 numbers 
+    
+    print "[#{num_1}]"
+    print "[#{num_2}]"
+    puts "[#{num_3}]"
+
+      if num_1 == num_2 && num_2 == num_3
+        puts 
+        puts "--- YOU WON THE JACKPOT! ---"
+        puts
+      elsif num1 == num3 
+        puts 
+        puts "---"
+      
+      # elsif num_1 == num_3
+      # elsif num_2 == num_3
+      # elsif num_1 == num_2
+      #   puts "YOu win 25000 $$"
+      # elsif num_1 + num_2 + num_3 == 8
+
+      # else 
+      #   puts "you lose money"
+      # end
+
+
+    # logic to see if you won or not
+
+
+    # # call another method result
+    # def results
+   
+      end 
+  end
+end
+Game.new
